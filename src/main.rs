1c243ca95988f48c090f3c4eab382b2a31f03899
Sony AK <sony@sony-ak.com>
2024-02-29 19:11:50 +0700

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,47 @@
+use hyper::service::{make_service_fn, service_fn};
+use hyper::StatusCode;
+use hyper::{Body, Request, Response, Server};
 use std::convert::Infallible;
+use std::future::Future;
 use std::net::SocketAddr;
+use std::pin::Pin;
 
-use http_body_util::Full;
-use hyper::body::Bytes;
-use hyper::server::conn::http1;
-use hyper::service::service_fn;
-use hyper::{Request, Response};
-use hyper_util::rt::TokioIo;
-use tokio::net::TcpListener;
+async fn hello(_: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new(Body::from("Hello, World!")))
+}
 
-async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+async fn about(_: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new(Body::from("This is about page")))
+}
+
+async fn not_found(_: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body(Body::empty())
+        .unwrap())
+}
+
+fn router(
+    req: Request<Body>,
+) -> Pin<Box<dyn Future<Output = Result<Response<Body>, Infallible>> + Send + 'static>> {
+    match (req.method(), req.uri().path()) {
+        (&hyper::Method::GET, "/") => Box::pin(hello(req)),
+        (&hyper::Method::GET, "/about") => Box::pin(about(req)),
+        _ => Box::pin(not_found(req)),
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr = SocketAddr::from(([127, 0, 0, 1], 3021));
 
-    // We create a TcpListener and bind it to 127.0.0.1:3021
-    let listener = TcpListener::bind(addr).await?;
+    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(router)) });
 
-    // We start a loop to continuously accept incoming connections
-    loop {
-        let (stream, _) = listener.accept().await?;
+    let server = Server::bind(&addr).serve(make_svc);
 
-        // Use an adapter to access something implementing `tokio::io` traits as if they implement
-        // `hyper::rt` IO traits.
-        let io = TokioIo::new(stream);
+    println!("Listening on http://{}", addr);
 
-        // Spawn a tokio task to serve multiple connections concurrently
-        tokio::task::spawn(async move {
-            // Finally, we bind the incoming connection to our `hello` service
-            if let Err(err) = http1::Builder::new()
-                // `service_fn` converts our function in a `Service`
-                .serve_connection(io, service_fn(hello))
-                .await
-            {
-                println!("Error serving connection: {:?}", err);
-            }
-        });
-    }
+    server.await?;
+
+    Ok(())
 }
