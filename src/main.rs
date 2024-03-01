8aa0cda87802a6908e557eeba05d84961403b746
Sony AK <sony@sony-ak.com>
2024-03-01 08:26:59 +0700

    Refactor code to use Actix-web framework

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,28 @@
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server, StatusCode};
-use serde_json::{json, to_string};
-use std::convert::Infallible;
-use std::future::Future;
-use std::net::SocketAddr;
-use std::pin::Pin;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-pub async fn index(_: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new(Body::from("Hello, World!")))
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }
 
-pub async fn about(_: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new(Body::from("This is about page")))
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
 }
 
-pub async fn upload(_: Request<Body>) -> Result<Response<Body>, Infallible> {
-    unimplemented!()
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
 }
 
-pub async fn json(_: Request<Body>) -> Result<Response<Body>, Infallible> {
-    let data = json!({
-        "message": "This is a sample JSON response",
-        "timestamp": 1234567890,
-        "status": "ok"
-    });
-
-    let json_str = to_string(&data).unwrap();
-
-    Ok(Response::builder()
-        .header("Content-Type", "application/json")
-        .body(Body::from(json_str))
-        .unwrap())
-}
-
-pub async fn not_found(_: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::builder()
-        .status(StatusCode::NOT_FOUND)
-        .body(Body::empty())
-        .unwrap())
-}
-
-fn router(
-    req: Request<Body>,
-) -> Pin<Box<dyn Future<Output = Result<Response<Body>, Infallible>> + Send + 'static>> {
-    match (req.method(), req.uri().path()) {
-        (&hyper::Method::GET, "/") => Box::pin(index(req)),
-        (&hyper::Method::GET, "/about") => Box::pin(about(req)),
-        (&hyper::Method::POST, "/upload") => Box::pin(upload(req)),
-        (&hyper::Method::GET, "/json") => Box::pin(json(req)),
-        _ => Box::pin(not_found(req)),
-    }
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3021));
-
-    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(router)) });
-
-    let server = Server::bind(&addr).serve(make_svc);
-
-    println!("Listening on http://{}", addr);
-
-    server.await?;
-
-    Ok(())
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 3021))?
+    .run()
+    .await
 }
