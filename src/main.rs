48fc7f3825248d60fe964dd020ee12883951e559
Sony AK <sony@sony-ak.com>
2024-03-01 08:34:17 +0700

    Add serde_json dependency and update routes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
-async fn hello() -> impl Responder {
+async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
@@ -10,17 +10,30 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
-async fn manual_hello() -> impl Responder {
+async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
+}
+
+async fn json() -> impl Responder {
+    let data = serde_json::json!({
+        "message": "This is a sample JSON response",
+        "timestamp": 1234567890,
+        "status": "ok"
+    });
+
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body(data.to_string())
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
+            .service(index)
             .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .route("/hello", web::get().to(hello))
+            .route("/json", web::get().to(json))
     })
     .bind(("127.0.0.1", 3021))?
     .run()
